cf57148bf86d5ce0cf5d49c2c1bec5e025f94a11
minho <radlohead@naver.com>
2019-04-02 12:58:39 +0900

    rootSaga all effects apply

--- src/sagas/index.ts
+++ src/sagas/index.ts
@@ -1,7 +1,10 @@
+import { all } from 'redux-saga/effects';
 import fetchCouponList from './CouponsApi';
 import fetchProductList from './ProductsApi';
 
 export default function* rootSaga() {
-    yield fetchProductList,
-    yield fetchCouponList
+    yield all([
+        fetchProductList,
+        fetchCouponList
+    ])
 }
