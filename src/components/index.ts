b4b9ac5def890afb451b340fd8fae53c7877e7bd
minho <radlohead@naver.com>
2019-04-07 09:32:20 +0900

    wishList, utils folder add

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,8 @@
-export { default as CouponAsync } from './coupon/CouponAsync';
-export { default as Coupon } from './coupon/Coupon';
-export { default as Home } from './Home';
-export { default as Menu } from './Menu';
-export { default as ProductsAsync } from './products/ProductsAsync';
-export { default as Products } from './products/Products';
-export { default as WishList } from './WishList';
-export { default as Spinners } from './Spinners';
+export { default as CouponAsync } from './coupon/CouponAsync'
+export { default as Coupon } from './coupon/Coupon'
+export { default as Home } from './Home'
+export { default as Menu } from './Menu'
+export { default as ProductsAsync } from './products/ProductsAsync'
+export { default as Products } from './products/Products'
+export { default as WishList } from './wishList/WishList'
+export { default as Spinners } from './utils/Spinners'
