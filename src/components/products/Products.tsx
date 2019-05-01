f2be8b822051ea313e7ad155d76deef98c90104e
minho <radlohead@naver.com>
2019-05-01 15:52:59 +0900

    totalPrice to WishList is Products change

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators, Dispatch } from 'redux'
-import { updateProducts } from '@src/actions'
+import { updateProducts, updateTotalPrice } from '@src/actions'
 import * as Types from '../../types/components/ProductsTypes'
 import ProductsImages from './ProductsImages'
 import '@src/css/components/Products.scss'
@@ -9,6 +9,7 @@ import '@src/css/components/Products.scss'
 interface IProductsProps {
     productsJSON: Types.IProductsJSON
     onUpdateProducts(productsJSON: Types.IProductsJSON): Types.IUpdateProducts
+    onUpdateTotalPrice(totalPrice: number): void
 }
 
 interface IProductsState {
@@ -37,10 +38,30 @@ class Products extends React.PureComponent<IProductsProps, IProductsState> {
         })
     }
 
+    private totalPrice(): void {
+        const { productsJSON, onUpdateTotalPrice } = this.props
+        const totalPrice = productsJSON
+            .map((v: Types.IProductsCountJSON['productsJSON']) => {
+                if (v.isInWishList) return v
+            })
+            .filter((v: Types.IProductsCountJSON['productsJSON']) => {
+                if (v) return v
+            })
+            .reduce(
+                (p: [], c: Types.IProductsCountJSON['productsJSON']) => {
+                    return [...p, c.price]
+                },
+                [0]
+            )
+            .reduce((p: number, c: number) => p + c)
+        onUpdateTotalPrice(totalPrice)
+    }
+
     private handleClickWishListAdd(item: Types.IProductsJSON['productsJSON']) {
         const { productsJSON, onUpdateProducts } = this.props
         item.isInWishList = !item.isInWishList
         onUpdateProducts(productsJSON)
+        this.totalPrice()
     }
 
     private handleClickWishListRemove(
@@ -51,6 +72,7 @@ class Products extends React.PureComponent<IProductsProps, IProductsState> {
         item.isChecked = false
         item.isInWishList = false
         onUpdateProducts(productsJSON)
+        this.totalPrice()
     }
 
     public renderProductItems(): JSX.Element {
@@ -115,13 +137,20 @@ class Products extends React.PureComponent<IProductsProps, IProductsState> {
     }
 }
 
+const mapStateToProps = (state: any) => {
+    return {
+        totalPrice: state.wishList.totalPrice
+    }
+}
+
 const mapDispatchToProps = (dispatch: Dispatch) => {
     return {
-        onUpdateProducts: bindActionCreators(updateProducts, dispatch)
+        onUpdateProducts: bindActionCreators(updateProducts, dispatch),
+        onUpdateTotalPrice: bindActionCreators(updateTotalPrice, dispatch)
     }
 }
 
 export default connect(
-    null,
+    mapStateToProps,
     mapDispatchToProps
 )(Products)
